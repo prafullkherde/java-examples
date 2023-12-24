ae855aa4972b2b1b27dd3b2db10b0bf515754c84
prafullkherde <prafull.kherde@gmail.com>
2023-12-24 09:47:58 +0530

    first example

---
+++ basic-examples/Hellow.java
@@ -0,0 +1,14 @@
+/**
+ * @author Praf
+ *
+ */
+public class Main {
+  public static void main(String[] args) {
+  	String name = "Prafull";
+    	sayHelloTo(name);
+  }
+  
+  public static void sayHelloTo(String name) {
+  	System.out.println("Hello World "+name);
+  }  
+}
